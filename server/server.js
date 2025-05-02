a42d06ba1cc2db583cad634c25fef426bbb0905d
tharushi <thswalpola@gmail.com>
2025-05-03 00:54:56 +0530

    main feature

--- server/server.js
+++ server/server.js
@@ -254,7 +254,11 @@ mongoose
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   
+  
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+
     
 app.use("/api/recipes", recipeRoutes);
 
