d1ce302a98fa9b591da6a07ba627ad36ea621181
Kaushal <Kaushalsilva15@gmail.com>
2025-05-03 07:52:26 +0530

    Update MealPlanningDateDisplay.jsx
    updated

--- client/src/pages/MealPlanningDateDisplay.jsx
+++ client/src/pages/MealPlanningDateDisplay.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const MealPlanningDateDisplay = () => {
   return (
-   
-    <div className="px-6"> {/* Added horizontal padding */}
-        <div>
-           <h1 className="text-xl font-bold mb-4 text-green-800">Meal Planning Details Form</h1>
-        </div>
+    <div className="px-6">
+      <div>
+        <h1 className="text-xl font-bold mb-4 text-green-800 text-center">
+          Meal Scheduling Details 
+        </h1>
+      </div>
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-             
               <th scope="col" className="px-6 py-3">
                 Breakfast Time
               </th>
@@ -24,7 +24,7 @@ const MealPlanningDateDisplay = () => {
             </tr>
           </thead>
           <tbody>
-            
+            {/* Table body goes here */}
           </tbody>
         </table>
       </div>
